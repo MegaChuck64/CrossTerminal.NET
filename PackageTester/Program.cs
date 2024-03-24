cdf91f00e39f9986191bd551003b978a8659e419
MegaChuck64 <cjscottcarey@yahoo.com>
2024-03-23 22:37:49 -0400

    new packcage tester project

---
+++ PackageTester/Program.cs
@@ -0,0 +1,44 @@
+﻿using CrossTermLib;
+using System.Numerics;
+
+namespace PackageTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var console = new ConsoleGL(
+                 cols: 60,
+                 rows: 30,
+                 fontPath: "Ubuntu.ttf",
+                 fontSize: 20,
+                 title: "Console GL Test",
+                 defaultFontColor: new Vector4(0f, 1f, 0.2f, 1f));
+
+
+            string name;
+            do
+            {
+                console.Clear();
+
+                console.WriteLine("Hello world...");
+                console.WriteLine("Please type your name...");
+
+                name = console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(name))
+                    continue;
+
+                console.WriteLine($"You entered '{name}'. Keep? y or n");
+                if (console.ReadLine() == "y")
+                    break;
+
+            } while (!console.IsClosing);
+
+            console.WriteLine($"Welcome, {name}. Press enter to exit...");
+
+            console.ReadLine();
+
+        }
+    }
+}
